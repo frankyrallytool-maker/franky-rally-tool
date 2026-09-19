b01a5ca2e2432a06d6d1f3fe9500368bdf452773
frankyrallytool-maker <frankyrallytool@gmail.com>
2026-09-19 11:52:12 +0400

    Bump build and clear stale mobile poster overlay

--- live-data.js
+++ live-data.js
@@ -1,6 +1,6 @@
 (function () {
   const API_URL = "https://script.google.com/macros/s/AKfycbxuxysWcVsk_Y6eARCGne_iH-hGUOSkAa2bkTuDLGXU9jgJ1sJPgz58Q41Cf0UcVo8svA/exec";
-  const APP_BUILD = "1.11.1";
+  const APP_BUILD = "1.11.2";
   const CACHE_KEY = "franky_sheet_cache_v3";
   const CACHE_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
 
@@ -745,6 +745,9 @@
     };
     document.head.appendChild(script);
   }
+
+  const stalePoster = document.getElementById("frankyPosterPreview");
+  if (stalePoster && stalePoster.parentNode) stalePoster.parentNode.removeChild(stalePoster);
 
   addLiveStyles();
   ensureRallySelectorOnTop();
