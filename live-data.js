e66690d700c47b4cb4f51be1cdbda9f083cc59a4
frankyrallytool-maker <frankyrallytool@gmail.com>
2026-09-19 16:48:13 +0400

    Bump app build to V1.12.0

--- live-data.js
+++ live-data.js
@@ -1,6 +1,6 @@
 (function () {
   const API_URL = "https://script.google.com/macros/s/AKfycbxuxysWcVsk_Y6eARCGne_iH-hGUOSkAa2bkTuDLGXU9jgJ1sJPgz58Q41Cf0UcVo8svA/exec";
-  const APP_BUILD = "1.11.2";
+  const APP_BUILD = "1.12.0";
   const CACHE_KEY = "franky_sheet_cache_v3";
   const CACHE_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
 
